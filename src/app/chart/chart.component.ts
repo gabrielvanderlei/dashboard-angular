a7814cb3fdf4c0156feaa8eeeef38a0873f9b551
gabrielvanderlei <gabrielvanderlei10@hotmail.com>
2019-08-18 00:01:45 -0300

    Some improvements.

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -62,7 +62,7 @@ export class ChartComponent implements OnInit {
           "data":{
             "labels": [],
             "datasets":[{
-              "label":"Sales in the month","data":[],
+              "label":"Sales By Month for (Selected Filters)","data":[],
               "fill":false,
               "backgroundColor":["rgba(255, 99, 132, 0.2)","rgba(255, 159, 64, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],
               "borderColor":["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],
